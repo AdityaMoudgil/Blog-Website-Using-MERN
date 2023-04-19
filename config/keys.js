46c681ee0c91ae287c694a27f6bfea235df94bff
Aditya Moudgil <moudgiladitya08@gmail.com>
2023-04-19 12:53:29 +0530

    Uploaded the project

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    //mongoURI: `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0-uafhs.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+    mongoURI: `mongodb+srv://harshit:asdqwe123@cluster0.ukdmwwr.mongodb.net`
+}
